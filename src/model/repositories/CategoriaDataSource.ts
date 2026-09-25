6f2144ee515f069d729c2e2205802999f5c4e8d0
DouglasLeone <douglasleone09@gmail.com>
2026-09-24 22:13:27 -0300

    refactor(model/repositories): cria DataSources para categorias e produtos

---
+++ src/model/repositories/CategoriaDataSource.ts
@@ -0,0 +1,27 @@
+import { Categoria } from "../entities/Categoria";
+
+const DELAY_MS = 600;
+
+const CATEGORIAS: Categoria[] = [
+  {
+    id: "comidas",
+    nome: "Comidas",
+    corBorda: "#501673",
+    corSeta: "#501673",
+    imagem: "categoria-comidas",
+  },
+  {
+    id: "bebidas",
+    nome: "Bebidas",
+    corBorda: "#1b873f",
+    corSeta: "#1b873f",
+    imagem: "categoria-bebidas",
+  },
+];
+
+export class CategoriaDataSource {
+  async listarTodas(): Promise<Categoria[]> {
+    await new Promise((resolve) => setTimeout(resolve, DELAY_MS));
+    return [...CATEGORIAS];
+  }
+}
